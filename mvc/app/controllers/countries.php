fc31dca16be8a75fb01ffdace8743c45518848ac
Fabiandejong07 <81682500+Fabiandejong07@users.noreply.github.com>
2022-03-17 12:35:02 +0100

    tabel toegevoegd

--- mvc/app/controllers/countries.php
+++ mvc/app/controllers/countries.php
@@ -2,13 +2,13 @@
 
 class countries extends Controller {
     public function __construct() {
-       $this->countryModel = $this->model('Country');
+       $this->countryModel = $this->model('country');
     }
     public function index() {
         $countrie = $this->countryModel->getCountries();
         $data = [
        'title' => 'Home page',
-       'countrie' => $countrie
+       'country' => $countrie
         ];
         $this->view('countries/index', $data);
     
--- mvc/app/views/countries/index.php
+++ mvc/app/views/countries/index.php
@@ -1,7 +1,43 @@
 <?php 
- echo '<a href=http://www.mvc-toets.org/countries/index>Click here</a>';
-  foreach ($data['Country'] as $countrie) {
+
+  foreach ($data['country'] as $countrie) {
    echo "informatien:" . $countrie->id . $countrie->name . $countrie->capitalCity . $countrie->continent . $countrie->population;
    echo "<br>";
+    $data .= "<tr><th scope='row'>" . $countrie->id  . "</th>
+    <td>  . $countrie->id . </td>
+    <td> . $countrie->name  . </td>
+    <td> . $countrie->capitalCity . </td>
+    <td> .  $countrie->continent .</td>
+    <td>  .  $countrie->population . </td>
+
+
+    ";
   }
 ?>
+
+
+<!doctype html>
+<html>
+<head>
+
+<meta name="description" content="Our first page">
+<meta name="keywords" content="html tutorial template">
+</head>
+<body>
+<table>
+  <tr>
+  <th>id</th>
+  <th>name</th>
+    <th>hoofdstad</th>
+    <th>continet</th>
+    <th>populatie</th>
+  </tr>
+  <tr>
+   <?php echo $data; ?>
+  </tr>
+ 
+</table>
+</body>
+</html>
+
+
